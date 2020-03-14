23ff2fbc60f3ab0858d6d4324943c4b7511d732b
sanderdebr <sander606@gmail.com>
2020-03-14 11:19:52 +0100

    update

---
+++ src/app.js
@@ -0,0 +1,12 @@
+import "./css/style.scss";
+
+const berend = "hoi";
+
+function steen() {
+  const beer = 2;
+  return beer;
+}
+
+steen();
+
+document.body.innerText = berend;
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,39 @@
 const path = require("path");
 
 module.exports = {
-  entry: "./src/index.js",
+  mode: "development",
+  entry: "./src/app.js",
   output: {
-    filename: "main.js",
+    filename: "bundle.js",
     path: path.resolve(__dirname, "dist")
+  },
+  devServer: {
+    contentBase: "./dist"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(s*)css$/,
+        use: [
+          "style-loader",
+          { loader: "css-loader", options: { importLoaders: 1 } },
+          "postcss-loader",
+          "sass-loader"
+        ]
+      },
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: [
+          {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env"]
+            }
+          },
+          "eslint-loader"
+        ]
+      }
+    ]
   }
 };
